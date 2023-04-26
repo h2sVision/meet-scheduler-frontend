13cf4cfca3cdc3c077c5c83c67973031fbc1ef65
saxenaprachi <prachi@hack2skill.com>
2023-04-26 14:34:28 +0530

    modified- login.js - button of google auth changed

--- pages/auth/login.js
+++ pages/auth/login.js
@@ -283,7 +283,7 @@ export default function Login() {
                         <h2 className="text-3xl font-semibold pt-5">Login to Schedule Interview Slot</h2>
                         <div className="flex w-full pt-8 items-center">
                         <Link href={`${process.env.NEXT_PUBLIC_BACKEND_API}/auth/google`} className="w-full">
-                          <button className="bg-white border-solid border-2 border-google-blue text-google-blue rounded-md font-normal  focus:outline-none focus:shadow-outline  flex gap-2 justify-center items-center w-full lg:w-96 py-2"> 
+                          <button className="bg-white shadow text-gray rounded-md font-normal  focus:outline-none focus:shadow-outline  flex gap-2 justify-center items-center w-full lg:w-96 py-2"> 
                             <img src="/auth/Google__G__Logo.png" width="20"/> Login in with Google 
                           </button>
                         </Link>
