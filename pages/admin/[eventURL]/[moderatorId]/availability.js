2b9c19f5c588f1a5c7ce17b3cc2aefe87caed700
saxenaprachi <prachi@hack2skill.com>
2023-04-27 10:57:08 +0530

    modified- availibilty.js - set time function executed

--- pages/admin/[eventURL]/[moderatorId]/availability.js
+++ pages/admin/[eventURL]/[moderatorId]/availability.js
@@ -16,8 +16,11 @@ import Link from 'next/link';
 import FullCalendar from '@fullcalendar/react' // must go before plugins
 import dayGridPlugin from '@fullcalendar/daygrid' // a plugin!
 import interactionPlugin from "@fullcalendar/interaction"
-import EditRoundedIcon from '@mui/icons-material/EditRounded';
-
+import EditRoundedIcon from '@mui/icons-material/EditRounded';  
+import AddCircleOutlineIcon from '@mui/icons-material/AddCircleOutline';
+import DeleteIcon from '@mui/icons-material/Delete';
+// Modal
+import Modal from '@mui/material/Modal';
 
 const Availability = () => {
   const router = useRouter();
@@ -115,6 +118,67 @@ const Availability = () => {
     }
   }
 
+  function convertTo24Hour(timeString) {
+      const [hours, minutes, indicator] = timeString.split(/:| /);
+      let hoursNum = Number(hours);
+      if (indicator === 'PM' && hoursNum !== 12) {
+        hoursNum += 12;
+      }
+      if (indicator === 'AM' && hoursNum === 12) {
+        hoursNum = 0;
+      }
+      return `${hoursNum.toString().padStart(2, '0')}:${minutes.padStart(2, '0')}`;
+  }
+
+  const setTime = async()=>{
+    let allIntputs = document.getElementsByClassName('clanedarTimeIntervals')[0].getElementsByTagName('input');
+    let hours=[];
+    for(let i=0; i< allIntputs.length;i+=2){
+        let newInterval = {
+            start: new Date(new Date().toDateString() +' '+ allIntputs[i].value).toISOString(),
+            end: new Date(new Date().toDateString()+' ' + allIntputs[i+1].value).toISOString(),
+        }
+        hours.push(newInterval);
+    }
+    let customHour={
+        date: activeDate,
+        hours: hours
+    }
+    console.log('custom Hour: ', customHour);
+    const response = await axiosPrivate.post(`/admin/${window.location.href.split('/')[4]}/${window.location.href.split('/')[5]}/customHours`,JSON.stringify({customHour: customHour}),{
+        headers: {
+            "Content-Type": "application/json"
+        },
+        withCredentials: true
+    });
+    fetchData();
+    setEventOpen(false);
+  }
+
+  const addTimeIntervals =(e)=>{
+    const item = document.createElement('div');
+    item.innerHTML =`
+      <div class='flex flex-col gap-2 w-48'>
+          <label class='text-dark-blue font-bold'>From</label>  
+          <input type='time' class='border-2 border-solid border-border-gray rounded p-1'/>
+      </div>
+      <div class='flex flex-col gap-2 w-48'>
+          <label class='text-dark-blue font-bold'>To</label>  
+          <input type='time' class='border-2 border-solid border-border-gray rounded p-1'/>
+      </div>
+      <div class='flex justify-center items-end py-2 cursor-pointer' onclick="this.parentNode.parentNode.removeChild(this.parentNode)">
+          <svg xmlns="http://www.w3.org/2000/svg" x="0px" y="0px"
+          width="24" height="24"
+          viewBox="0 0 24 24">
+              <path d="M 10 2 L 9 3 L 5 3 C 4.448 3 4 3.448 4 4 C 4 4.552 4.448 5 5 5 L 7 5 L 17 5 L 19 5 C 19.552 5 20 4.552 20 4 C 20 3.448 19.552 3 19 3 L 15 3 L 14 2 L 10 2 z M 5 7 L 5 20 C 5 21.105 5.895 22 7 22 L 17 22 C 18.105 22 19 21.105 19 20 L 19 7 L 5 7 z"></path>
+          </svg>
+      </div>`;
+      item.classList.add('flex');
+      item.classList.add('gap-4');
+      e.target.parentNode.children[0].append(item);
+  }
+
+  // Calendar Funstions
   function formatDate(date) {
       const year = date.getFullYear();
       const month = String(date.getMonth() + 1).padStart(2, '0'); 
@@ -172,14 +236,76 @@ const Availability = () => {
     <LoggedinLayout leftSidebarOpen={leftSidebarOpen} setLeftSidebarOpen={setLeftSidebarOpen} fetchData={fetchData}>
       <div className='w-full flex flex-col py-3 justify-center items-center'>
         <div className='w-11/12 flex justify-start'><div><Link href={`/admin/${event?.eventURL}`}>Back</Link></div></div>
-        <div className='w-11/12 p-5'>
-          <FullCalendar
-            plugins={[ dayGridPlugin, interactionPlugin ]}
-            initialView="dayGridMonth"
-            events={events}
-            eventContent={eventContent}
-          />
-        </div>
+        {availability?.length?(
+          <div className='w-11/12 p-5'>
+            <FullCalendar
+              plugins={[ dayGridPlugin, interactionPlugin ]}
+              initialView="dayGridMonth"
+              events={events}
+              eventClick={handleEventClick}
+              eventContent={eventContent}
+            />
+            <Modal
+              open={eventOpen}
+              onClose={()=>{setEventOpen(false)}}
+              className='flex justify-center items-center'>
+              <div className='w-3/5 h-96 bg-white border-0 outline-0 rounded-lg p-8 flex'>
+                <div className='w-1/2 flex flex-col gap-3 pr-3 text-xl font-bold'>
+                  Change Time Availability
+                  <div className='flex flex-col gap-4 text-base font-medium clanedarTimeIntervals'>
+                      {timeIntervals.map((timeInterval, key)=>{
+                          return (
+                            <>
+                            {timeInterval != '' &&(
+                              <div className='flex gap-4' key={key}>
+                                <div className='flex flex-col gap-2 w-48'>
+                                  <label className='text-dark-blue font-bold'>From</label>  
+                                  <input type='time' defaultValue={convertTo24Hour(timeInterval.split('-')[0])} className='border-2 border-solid border-border-gray rounded p-1'/>
+                                </div>
+                                <div className='flex flex-col gap-2 w-48'>
+                                  <label className='text-dark-blue font-bold'>To</label>  
+                                  <input type='time' defaultValue={convertTo24Hour(timeInterval.split('-')[1])} className='border-2 border-solid border-border-gray rounded p-1'/>
+                                </div>
+                                  {key!=0 &&(
+                                    <div className='flex justify-center items-end py-2 cursor-pointer' >
+                                        <DeleteIcon onClick={(e)=>{ console.log(e.currentTarget);e.currentTarget.parentNode.parentNode.parentNode.removeChild(e.currentTarget.parentNode.parentNode)}}/> 
+                                    </div>
+                                  )}
+                              </div>
+                            )}
+                          </>
+                      )})}
+                      {timeIntervals.length===1 &&(
+                        <div className='flex gap-4'>
+                          <div className='flex flex-col gap-2 w-48'>
+                            <label className='text-dark-blue font-bold'>From</label>  
+                            <input type='time'  className='border-2 border-solid border-border-gray rounded p-1'/>
+                          </div>
+                          <div className='flex flex-col gap-2 w-48'>
+                            <label className='text-dark-blue font-bold'>To</label>  
+                            <input type='time' className='border-2 border-solid border-border-gray rounded p-1'/>
+                          </div>
+                        </div>
+                      )}
+                                          
+                  </div>
+                    <button onClick={addTimeIntervals} className='flex gap-3 items-center justify-center text-gray text-base font-medium'><AddCircleOutlineIcon/> Add more time intervals</button>
+                      <div className='flex gap-3 w-full justify-center items-center mt-5 text-base font-medium'>
+                        <button className='h2s-button' onClick={setTime}>Set Time</button>
+                        <button className='h2s-gray-button' onClick={()=>{setEventOpen(false)}}>Back</button>
+                      </div>
+                </div>
+                <div className='w-1/2 flex flex-col gap-5 pl-3'>
+                  <div className='text-xl'>Today Booked Conference</div>
+                  {/* <div><Table tableHeaders={['Time Interval', 'Email ID']} tableContent={conferences} tableName={'calendarBookedConferences'} activeDate={activeDate}/></div> */}
+                  <div className='w-full flex justify-center items-center text-sm'>To Reschedule the conference, contact the admin</div>
+                </div>
+              </div>
+            </Modal>
+          </div>
+        ):(
+          <div> Moderator has not thier availability yet</div>
+        )}
       </div>
     </LoggedinLayout>
     </>
