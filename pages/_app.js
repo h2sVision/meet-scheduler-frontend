e086940691e6d32d147d21a0e3a5b509fcefd853
Pratik Verma <40285992+rexyagami@users.noreply.github.com>
2023-05-01 11:13:06 +0530

    modified-appJS-added gtag

--- pages/_app.js
+++ pages/_app.js
@@ -1,11 +1,25 @@
 import '../styles/globals.css'
 import { Provider } from 'react-redux';
 import { store } from '../common/redux/store';
+import Head from 'next/head';
 
 export default function App({ Component, pageProps }) {
   return (
+    <>
+      <Head>
+        <script async src="https://www.googletagmanager.com/gtag/js?id=G-3RL18N80F9"></script>
+        <script dangerouslySetInnerHTML={{
+          __html: `
+            window.dataLayer = window.dataLayer || [];
+            function gtag(){dataLayer.push(arguments);}
+            gtag('js', new Date());
+            gtag('config', 'G-3RL18N80F9');
+          `
+        }} />
+      </Head>
       <Provider store={store}>
         <Component {...pageProps} />
       </Provider>
+    </>
   )
 }
